e1c0cf038c7d7fe3b205a81452cc30aaf789cdc3
spacefoul <lightturtle8@gmail.com>
2025-04-11 17:47:41 +0900

    2475: 검증수

---
+++ class_1/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4];
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i] * arr [i];
+    }
+
+    cout << sum << endl;
+}
