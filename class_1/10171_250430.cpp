850cf9e833e9a7cb7f25748c68c96b7c4b697811
spacefoul <lightturtle8@gmail.com>
2025-04-30 13:46:11 +0900

    10171: 고양이

---
+++ class_1/10171_250430.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "\\    /\\ \n";
+    cout << " )  ( ') \n";
+    cout << "(  /  ) \n";
+    cout << " \\(__)| \n";
+
+}
